d0f7b7ddbe4c14a458fa120e4cc418364bba2f2a
khanaljalapa02 <khanaljalapa02@gmail.com>
2026-03-31 17:50:56 +0545

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,23 @@ const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 const commands = [
   new SlashCommandBuilder()
     .setName('member-log')
-    .setDescription('Post an editable member log')
+    .setDescription('Post an editable member log'),
+
+  new SlashCommandBuilder()
+    .setName('add-trigger')
+    .setDescription('Add a trigger word and response')
+    .addStringOption(option => option.setName('trigger').setDescription('The trigger word or phrase').setRequired(true))
+    .addStringOption(option => option.setName('response').setDescription('The response to send').setRequired(true)),
+
+  new SlashCommandBuilder()
+    .setName('remove-trigger')
+    .setDescription('Remove a trigger word')
+    .addStringOption(option => option.setName('trigger').setDescription('The trigger word to remove').setRequired(true)),
+
+  new SlashCommandBuilder()
+    .setName('list-triggers')
+    .setDescription('List all trigger words for this server'),
+
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
