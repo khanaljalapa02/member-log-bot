4b250d4853d73f8d8134885e6c01a42018767f9c
khanaljalapa02 <khanaljalapa02@gmail.com>
2026-04-01 14:37:29 +0545

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,10 @@ const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 const commands = [
   new SlashCommandBuilder()
     .setName('member-log')
-    .setDescription('Post an editable member log')
+    .setDescription('Post an editable member log'),
+  new SlashCommandBuilder()
+    .setName('sticky-log')
+    .setDescription('Post a sticky editable log (always moves to bottom)')
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
